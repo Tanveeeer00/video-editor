11fb70a7ba342c67b67512c7e989d9107dd54c4b
Tanveer <shaikhtanveer5205@gmail.com>
2024-03-29 23:48:13 +0530

    sidebar data added

---
+++ src/app/dashboard/page.js
@@ -0,0 +1,14 @@
+import Button from "@/components/Button";
+import SearchBox from "@/components/SearchBox";
+import SideBar from "@/components/SideBar";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      {/* <SearchBox /> */}
+      {/* <Button className="">hello button</Button> */}
+      {/* <SideBar /> */}
+    </div>
+  );
+}
