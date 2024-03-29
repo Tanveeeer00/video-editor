2adfcae087eb8f4bfb34eb09e81c0caffd446dcc
Tanveer <shaikhtanveer5205@gmail.com>
2024-03-29 23:46:44 +0530

    add search box component

---
+++ src/components/SearchBox.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function SearchBox() {
+  return (
+    <form className="max-w-md mx-auto mt-5">
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+          <svg
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 20 20"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+            />
+          </svg>
+        </div>
+        <input
+          type="search"
+          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-3xl bg-gray-50 focus:ring-gray-500 focus:border-gray-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-gray-500 dark:focus:border-gray-500"
+          placeholder="Search..."
+          required
+        />
+      </div>
+    </form>
+  );
+}
