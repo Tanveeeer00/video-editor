45767a4d84f6b8238081ebc75aa0d3ae0c112e5f
Tanveer <shaikhtanveer5205@gmail.com>
2024-03-29 23:47:47 +0530

    add sidebar component

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,23 @@
+import { sidebarData } from "@/lib/data";
+import React from "react";
+import Button from "./Button";
+
+export default function SideBar() {
+  return (
+    <div className="bg-white h-screen w-[15rem] text-black flex flex-col items-center">
+      <div className="pt-5">
+        <p className="font-bold text-3xl">Veed.io</p>
+      </div>
+      <div className="flex flex-col mt-5">
+        {sidebarData.map((link) => (
+          <Button
+            key={link.id}
+            className="m-2 bg-transparent hover:bg-gray-300 text-gray-800 hover:shadow-md text-md hover:font-bold w-[13rem]"
+          >
+            {link.name}
+          </Button>
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ src/lib/data.js
@@ -0,0 +1,18 @@
+export const sidebarData = [
+  {
+    id: 1,
+    name: "New Video +",
+  },
+  {
+    id: 2,
+    name: "Home",
+  },
+  {
+    id: 3,
+    name: "Template",
+  },
+  {
+    id: 4,
+    name: "All Videos",
+  },
+];
