24d76fc794de91bc54cbf50c04191408ab6c88d5
Tanveer <shaikhtanveer5205@gmail.com>
2024-04-01 22:41:52 +0530

    fixes

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,23 +1,62 @@
-import { sidebarData } from "@/lib/data";
+import data from "@/lib/data.json";
 import React from "react";
-import Button from "./Button";
+// import Button from "./Button";
+import Image from "next/image";
 
 export default function SideBar() {
+  const sidebarData = data.sidebarData;
   return (
-    <div className="bg-white h-screen w-[15rem] text-black flex flex-col items-center">
-      <div className="pt-5">
-        <p className="font-bold text-3xl">Veed.io</p>
-      </div>
-      <div className="flex flex-col mt-5">
-        {sidebarData.map((link) => (
-          <Button
-            key={link.id}
-            className="m-2 bg-transparent hover:bg-gray-300 text-gray-700 hover:shadow-md text-md hover:font-bold w-[13rem]"
-          >
-            {link.name}
-          </Button>
-        ))}
-      </div>
-    </div>
+    <>
+      <button
+        data-drawer-target="logo-sidebar"
+        data-drawer-toggle="logo-sidebar"
+        aria-controls="logo-sidebar"
+        type="button"
+        class="inline-flex items-center p-2 mt-2 ms-3 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+      >
+        <span class="sr-only">Open sidebar</span>
+        <svg
+          class="w-6 h-6"
+          aria-hidden="true"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            clip-rule="evenodd"
+            fill-rule="evenodd"
+            d="M2 4.75A.75.75 0 012.75 4h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 4.75zm0 10.5a.75.75 0 01.75-.75h7.5a.75.75 0 010 1.5h-7.5a.75.75 0 01-.75-.75zM2 10a.75.75 0 01.75-.75h14.5a.75.75 0 010 1.5H2.75A.75.75 0 012 10z"
+          ></path>
+        </svg>
+      </button>
+      <aside
+        id="logo-sidebar"
+        class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
+        aria-label="Sidebar"
+      >
+        <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          <div class="flex items-center ps-2.5 mb-5">
+            <Image
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-6 me-3 sm:h-7"
+              width={52}
+              height={6}
+              alt="Flowbite Logo"
+            />
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Veed.Io
+            </span>
+          </div>
+
+          {sidebarData.map((link) => (
+            <ul class="space-y-2 font-medium" key={data.id}>
+              <li className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <span className="ms-3">{link.name}</span>
+              </li>
+            </ul>
+          ))}
+        </div>
+      </aside>
+    </>
   );
 }
