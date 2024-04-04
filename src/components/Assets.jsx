81380fa6c1bbf2b3913dd025488747bff3a0eef2
Tanveer <shaikhtanveer5205@gmail.com>
2024-04-04 14:35:28 +0530

    aside section component

---
+++ src/components/Assets.jsx
@@ -0,0 +1,74 @@
+import React from "react";
+import Container from "./Container";
+import data from "@/lib/data.json";
+import Image from "next/image";
+
+export default function Assets() {
+  const assetData = data.AssetData;
+  return (
+    <div className="flex flex-col justify-center items-center">
+      <p className="font-bold text-xl mt-3">Assets</p>
+      <div className="flex flex-col md:flex-row">
+        {assetData.map((project) => (
+          <div key={project.id} className="m-3">
+            <Image
+              height={200}
+              width={200}
+              alt={project.title}
+              src={project.Image}
+            />
+            <p>{project.title}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className=" grid grid-cols-2 w-full mb-3 absolute bottom-0">
+        <div className="col-span-1 flex justify-end mr-3">
+          <svg
+            className="h-auto w-auto bg-sky-600 text-gray-800 dark:text-white cursor-pointer rounded-sm"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M5 12h14m-7 7V5"
+            />
+          </svg>
+        </div>
+
+        <div className="col-span-1 flex justify-end md:mx-2 mx-1">
+          <svg
+            className=" h-6 w-6 mr-2 md:mr-4 cursor-pointer rounded-sm"
+            aria-hidden="true"
+            height="24"
+            width="24"
+            fill="white"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 448 512"
+          >
+            <path d="M128 136c0-22.1-17.9-40-40-40L40 96C17.9 96 0 113.9 0 136l0 48c0 22.1 17.9 40 40 40H88c22.1 0 40-17.9 40-40l0-48zm0 192c0-22.1-17.9-40-40-40H40c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40H88c22.1 0 40-17.9 40-40V328zm32-192v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V136c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM288 328c0-22.1-17.9-40-40-40H200c-22.1 0-40 17.9-40 40l0 48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V328zm32-192v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V136c0-22.1-17.9-40-40-40l-48 0c-22.1 0-40 17.9-40 40zM448 328c0-22.1-17.9-40-40-40H360c-22.1 0-40 17.9-40 40v48c0 22.1 17.9 40 40 40h48c22.1 0 40-17.9 40-40V328z" />
+          </svg>
+
+          <svg
+            className=" h-6 w-6 cursor-pointer rounded-sm"
+            aria-hidden="true"
+            height="24"
+            width="24"
+            fill="white"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path d="M40 48C26.7 48 16 58.7 16 72v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V72c0-13.3-10.7-24-24-24H40zM192 64c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zm0 160c-17.7 0-32 14.3-32 32s14.3 32 32 32H480c17.7 0 32-14.3 32-32s-14.3-32-32-32H192zM16 232v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V232c0-13.3-10.7-24-24-24H40c-13.3 0-24 10.7-24 24zM40 368c-13.3 0-24 10.7-24 24v48c0 13.3 10.7 24 24 24H88c13.3 0 24-10.7 24-24V392c0-13.3-10.7-24-24-24H40z" />
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
+}
